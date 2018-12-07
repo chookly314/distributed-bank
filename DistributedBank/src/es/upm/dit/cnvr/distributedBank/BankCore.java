5f1461783c8da4bdc98bd38d1a1ed43d32f98c42
mikirockerful <gamallomiguel@gmail.com>
2018-12-07 18:01:25 +0100

    Starter on UpdateManager and BankCore

--- DistributedBank/src/es/upm/dit/cnvr/distributedBank/BankCore.java
+++ DistributedBank/src/es/upm/dit/cnvr/distributedBank/BankCore.java
@@ -1,10 +1,56 @@
 package es.upm.dit.cnvr.distributedBank;
 
-public class BankCore {
+import org.apache.log4j.Logger;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
+public class BankCore {
+	
+	private static Logger logger = Logger.getLogger(ClusterManager.class);
+	private ZooKeeper zk;
+	private boolean leader;
+	
+	public BankCore () {
+		this.leader = true;
+		
+		//We will create a Zookeeper session for test purposes. This session should be obtained from other classes
+		try {
+			if (zk == null) {
+				zk = new ZooKeeper(ZookeeperServersEnum.getRandomServer(),
+						ConfigurationParameters.ZOOKEEPER_SESSION_TIMEOUT, sessionWatcher);
+				try {
+					// Wait for creating the session. Use the object lock
+					wait();
+				} catch (Exception e) {
+				}
+			}
+		} catch (Exception e) {
+			logger.error(
+					String.format("Error creating the session between the UpdateManager and Zookeeper", e.toString()));
+		}
+		
+		UpdateManager updatemanager = new UpdateManager(this, zk);
+
+	}
 	
 	public static void main(String[] args) {
 		
+		BankCore bankcore = new BankCore();
+		
 	}
+	
+	public boolean isLeader() {
+		return this.leader;
+	}
+	
+	// *** Watchers ***
+
+	private Watcher sessionWatcher = new Watcher() {
+		public void process(WatchedEvent e) {
+			logger.info(String.format("ClusterManager Zookeeper session created: {}.", e.toString()));
+			notify();
+		}
+	};
 	
 }
--- DistributedBank/src/es/upm/dit/cnvr/distributedBank/UpdateManager.java
+++ DistributedBank/src/es/upm/dit/cnvr/distributedBank/UpdateManager.java
@@ -1,5 +1,56 @@
 package es.upm.dit.cnvr.distributedBank;
 
-public class UpdateManager {
+import org.apache.log4j.Logger;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.ZooDefs.Ids;
 
+public class UpdateManager {
+	private static Logger logger = Logger.getLogger(ClusterManager.class);
+	private ZooKeeper zk;
+	private BankCore bankcore;
+	public UpdateManager(BankCore bankcore, ZooKeeper zk) {
+		
+		this.bankcore = bankcore;
+		this.zk = zk;
+		
+		
+		//Create a node for storing operations
+		try {
+			Stat s = zk.exists(ConfigurationParameters.ZOOKEEPER_TREE_OPERATIONS_ROOT, false);
+			if (s == null) {
+				zk.create(ConfigurationParameters.ZOOKEEPER_TREE_OPERATIONS_ROOT, new byte[0],
+						Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);	
+			}
+		} catch (Exception e)
+		{
+			logger.error("Error creating operations node");
+		}
+		
+		//Set watcher on that directory if server is not leader
+		if (!bankcore.isLeader()) {
+			try {
+				Stat s = zk.exists(ConfigurationParameters.ZOOKEEPER_TREE_OPERATIONS_ROOT, operationsWatcher);
+			} catch (Exception e) {
+				logger.error("Error setting watcher for operations node");
+			} 
+		}
+		
+		
+	}
+	
+	// *** Watchers ***
+	
+	// Notified of changes in the operations znode
+	private Watcher operationsWatcher = new Watcher() {
+		public void process(WatchedEvent event) {
+			
+		}
+	};
+	
+	
 }
